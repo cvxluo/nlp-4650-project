044c9e4eaa1546cbc67f9400d033bce56f68f0dc
samgamage <sammygamage@gmail.com>
2021-10-25 23:09:39 -0400

    Attempting to fix issues with GPT-2 training library

--- gpt2.py
+++ gpt2.py
@@ -1,6 +1,8 @@
 import logging
 import os
+from random import sample
 import time
+import math
 import datetime
 from typing import Dict
 
@@ -36,119 +38,121 @@ class TweetDataset(Dataset):
         with open(file_path, encoding="utf-8") as f:
             tweets = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
 
-        for tweet in tweets:
-            batch_encoding = tokenizer(tweet, truncation=True, max_length=block_size, padding="max_length")
-            self.attention_masks.append(torch.tensor(batch_encoding['attention_mask']))
-            self.examples.append(torch.tensor(batch_encoding["input_ids"]))
+        batch_encoding = tokenizer(tweets, truncation=True, max_length=block_size)
+        self.examples = batch_encoding["input_ids"]
+        self.examples = [{"input_ids": torch.tensor(e, dtype=torch.long)} for e in self.examples]
 
     def __len__(self) -> int:
         return len(self.examples)
 
     def __getitem__(self, i) -> Dict[str, torch.tensor]:
-        return self.examples[i], self.attention_masks[i]
+        return self.examples[i]
+
+
+
+def evaluate(model, device, iterator, log_output=True):
+    t0 = time.time()
+    model.eval()
+    total_eval_loss = 0
+
+    # Evaluate data for one epoch
+    for batch in iterator:
+        
+        input_ids = batch[0].to(device)
+        labels = batch[0].to(device)
+        attention_mask = batch[1].to(device)
+        
+        with torch.no_grad():        
+            outputs  = model(
+                input_ids, 
+                attention_mask=attention_mask,
+                labels=labels
+            )
+            loss = outputs[0]  
+            
+        batch_loss = loss.item()
+        total_eval_loss += batch_loss        
+
+    avg_val_loss = total_eval_loss / len(iterator)
+    val_perplexity = math.exp(avg_val_loss)
+    validation_time = format_time(time.time() - t0)
+
+    if log_output:
+        logger.info("Validation Loss: {0:.2f}".format(avg_val_loss))
+        logger.info("Validation perplexity: {0:.2f}".format(val_perplexity))
+        logger.info("Validation took: {:}".format(validation_time))
+
+    return avg_val_loss, val_perplexity
+
+
+def train_loop(model, device, optimizer, iterator, tokenizer, n_samples=1, sample_every=100, log_output=True):
+    t0 = time.time()
+    total_train_loss = 0
+    model.train()
+
+    for step, batch in enumerate(iterator):
+        input_ids = batch[0].to(device)
+        labels = batch[0].to(device)
+        attention_mask = batch[1].to(device)
+
+        model.zero_grad()        
+
+        outputs = model(
+            input_ids,
+            labels=labels, 
+            attention_mask=attention_mask
+        )
+
+        loss = outputs[0]
+        batch_loss = loss.item()
+        total_train_loss += batch_loss
+
+        # Get samples every x batches.
+        if step % sample_every == 0 and not step == 0:
+            elapsed = format_time(time.time() - t0)
+            logger.info("Batch {:>5,}  of  {:>5,}. Loss: {:>5,}.   Elapsed: {:}.".format(step, len(iterator), batch_loss, elapsed))
+
+            model.eval()
+            generate(model, device, tokenizer, n_sequences=n_samples)
+            model.train()
+
+        loss.backward()
+        optimizer.step()
+
+    avg_train_loss = total_train_loss / len(iterator)
+    train_perplexity = math.exp(avg_train_loss)
+    training_time = format_time(time.time() - t0)
+
+    if log_output:
+        logger.info("Average training loss: {0:.2f}".format(avg_train_loss))
+        logger.info("Training perplexity: {0:.2f}".format(train_perplexity))
+        logger.info("Training took: {:}".format(training_time))
+
+    return avg_train_loss, train_perplexity
 
 
 def train(model, device, optimizer, train_iterator, val_iterator, tokenizer,
-          epochs=5, sample_every=100, n_samples=1):
+          epochs=5, sample_every=100, n_samples=1, log_output=True):
     """Train the GPT-2 by fine-tuning the pretrained LM weights"""
 
     total_t0 = time.time()
     training_stats = []
 
     for epoch_i in range(0, epochs):
-        # ========================================
-        #               Training
-        # ========================================
-
         logger.info('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
         logger.info('Training...')
-
-        t0 = time.time()
-        total_train_loss = 0
-        generated_samples = []
-        model.train()
-
-        for step, batch in enumerate(train_iterator):
-            input_ids = batch[0].to(device)
-            labels = batch[0].to(device)
-            attention_mask = batch[1].to(device)
-
-            model.zero_grad()        
-
-            outputs = model(
-                input_ids,
-                labels=labels, 
-                attention_mask=attention_mask,
-                token_type_ids=None
-            )
-
-            loss = outputs[0]
-            batch_loss = loss.item()
-            total_train_loss += batch_loss
-
-            # Get sample every x batches.
-            if step % sample_every == 0 and not step == 0:
-                elapsed = format_time(time.time() - t0)
-                logger.info("Batch {:>5,}  of  {:>5,}. Loss: {:>5,}.   Elapsed: {:}.".format(step, len(train_iterator), batch_loss, elapsed))
-
-                model.eval()
-                samples = generate(model, device, tokenizer, n_sequences=n_samples)
-                generated_samples.append(samples)
-                model.train()
-
-            loss.backward()
-            optimizer.step()
-
-        # Calculate the average loss over all of the batches.
-        avg_train_loss = total_train_loss / len(train_iterator)       
-        
-        # Measure how long this epoch took.
-        training_time = format_time(time.time() - t0)
-
-        logger.info("Average training loss: {0:.2f}".format(avg_train_loss))
-        logger.info("Training epoch took: {:}".format(training_time))
-            
-        # ========================================
-        #               Validation
-        # ========================================
-
-        logger.info("Running Validation...")
-
-        t0 = time.time()
-        model.eval()
-        total_eval_loss = 0
-
-        # Evaluate data for one epoch
-        for batch in val_iterator:
-            
-            input_ids = batch[0].to(device)
-            labels = batch[0].to(device)
-            attention_mask = batch[1].to(device)
-            
-            with torch.no_grad():        
-                outputs  = model(input_ids, 
-                                attention_mask=attention_mask,
-                                labels=labels)
-                loss = outputs[0]  
-                
-            batch_loss = loss.item()
-            total_eval_loss += batch_loss        
-
-        avg_val_loss = total_eval_loss / len(val_iterator)
-        validation_time = format_time(time.time() - t0)    
-
-        logger.info("Validation Loss: {0:.2f}".format(avg_val_loss))
-        logger.info("Validation took: {:}".format(validation_time))
-
+        train_loss, train_perplexity = train_loop(model, device, optimizer, train_iterator, tokenizer, n_samples=n_samples,
+                                                  sample_every=sample_every, log_output=log_output)
+        logger.info("Evaluating...")
+        val_loss, val_perplexity = evaluate(model, device, val_iterator, log_output=log_output)
         # Record all statistics from this epoch.
         training_stats.append(
             {
-                'epoch': epoch_i + 1,
-                'Training Loss': avg_train_loss,
-                'Valid. Loss': avg_val_loss,
-                'Training Time': training_time,
-                'Validation Time': validation_time,
-                'Samples': generated_samples,
+                'Epoch': epoch_i + 1,
+                'Training Loss': train_loss,
+                'Training Perplexity': train_perplexity,
+                'Eval Loss': val_loss,
+                'Eval Perplexity':  val_perplexity,
             }
         )
 
@@ -225,13 +229,13 @@ def save_model(username: str, model, tokenizer, output_dir="models"):
     logger.info(f"Done saving. Model is located at {output_dir}.")
 
 
-def load_model(username: str, models_dir="models", print_details=True):
+def load_model(username: str, device, models_dir="models", print_details=True):
     """Loads a pretrained model from local models"""
     model_dir = os.path.join(models_dir, username)
 
     logger.info(f"Loading model from {models_dir}...")
 
-    model = GPT2LMHeadModel.from_pretrained(model_dir)
+    model = GPT2LMHeadModel.from_pretrained(model_dir).to(device)
     tokenizer = GPT2Tokenizer.from_pretrained(model_dir)
 
     logger.info(f"Loaded fine-tuned GPT-2 model for {username}")
