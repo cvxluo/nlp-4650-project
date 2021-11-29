0428e7d61c90e6e0928b7accbe27ba5de983025a
samgamage <sammygamage@gmail.com>
2021-11-29 12:22:46 -0500

    fix: Achieve somewhat better output

--- dataset.py
+++ dataset.py
@@ -8,8 +8,12 @@ def preprocess(text):
     link_pattern = r"\w+.(com|org|gov)\n.*\n.*"
     video_metadata_pattern = r"(\d+(\.\d+)?(K|M)?) views.*"
     video_duration_pattern = r"\d:\d+"
-    metadata_pattern = r"\n(\d+(\.\d+)?(K|M)?)\n(\d+(\.\d+)?(K|M)?)(\n(\d+(.\d+)?(K|M)?))?"
-    only_metdata_pattern = r"(\d+(\.\d+)?(K|M)?)\n(\d+(\.\d+)?(K|M)?)\n(\d+(\.\d+)?(K|M)?)"
+    metadata_pattern = (
+        r"\n(\d+(\.\d+)?(K|M)?)\n(\d+(\.\d+)?(K|M)?)(\n(\d+(.\d+)?(K|M)?))?"
+    )
+    only_metdata_pattern = (
+        r"(\d+(\.\d+)?(K|M)?)\n(\d+(\.\d+)?(K|M)?)\n(\d+(\.\d+)?(K|M)?)"
+    )
     thread_pattern = r"Show this thread"
     embedded_tweet_pattern = r"\n(.*)\n@\w*\n · .*"
     poll_pattern = r"\n.*\n(\d+(\.\d+)?)%.*"
@@ -34,28 +38,25 @@ def preprocess(text):
 def preprocess_dataset(df: pd.DataFrame):
     is_not_rt = ~df["Embedded_text"].str.contains("RT")
     is_not_reply = ~df["Embedded_text"].str.contains("Replying to")
-    is_not_unavailable = ~df["Emebdded_text"].str.contains("This Tweet is unavailable")
+    is_not_unavailable = ~df["Embedded_text"].str.contains("This Tweet is unavailable")
 
-    pipeline = [
-        is_not_rt,
-        is_not_reply,
-        is_not_unavailable
-    ]
+    pipeline = [is_not_rt, is_not_reply, is_not_unavailable]
 
     for filter in pipeline:
         df = df[filter]
 
-    df['text'] = df['Embedded_text'].apply(preprocess)
-    return df[['text']]
+    df["text"] = df["Embedded_text"].apply(preprocess)
+    return df[["text"]]
 
-def load_dataset(username: str, split=[.8, .1, .1], data_dir='data'):
-    assert round(sum(split), 2) == 1 and len(split) >= 2
-    train_df, test_df, val_df = None, None, None
-    df = pd.read_csv(f'{data_dir}/{username}.csv')
+
+def load_dataset(username: str, data_dir="data"):
+    df = pd.read_csv(f"{data_dir}/{username}.csv")
     df = preprocess_dataset(df)
-    train_split = split[0]
-    val_split = split[1]
-    train_end = int(train_split * len(df.index))
-    validate_end = int(val_split * len(df.index)) + train_end
-    train_df, val_df, test_df = df.iloc[:train_end], df.iloc[train_end:validate_end], df.iloc[validate_end:]
-    return train_df, val_df, test_df
+    return df
+
+
+def split_train_val(df: pd.DataFrame, props=[0.8, 0.2]):
+    assert round(sum(props), 2) == 1 and len(props) == 2
+    train_end = int(props[0] * len(df.index))
+    train_df, val_df = df.iloc[:train_end], df.iloc[train_end:]
+    return train_df, val_df
